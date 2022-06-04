314719545f19c6713446c6aa9498f7067d5f3d04
annaantonn <105316891+annaantonn@users.noreply.github.com>
2022-06-03 17:22:23 -0700

    Add files via upload

---
+++ p2-expressions.js
@@ -0,0 +1,68 @@
+/*
+    CIT 281 Project 2
+    Name: Anna Anton
+*/
+
+// // Returns a random number between min (inclusive) and max (exclusive)
+// function getRandomInteger(min, max) {
+//     return Math.floor(Math.random() * (max - min) + min);
+// }
+
+// // return a random letter
+// function getRandomLetter() {
+//     const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+//     return alphabet[getRandomInteger(0,alphabet.length)];
+// }
+
+
+// // will return the random length string
+// function getRandomString(minLength, maxLength) {
+//     let result = "";
+
+//     let lengthOfOutputString = getRandomInteger(minLength, maxLength);
+
+//     for (let i = 0; i < lengthOfOutputString; i++) {
+//         result += getRandomLetter();
+//     }
+//     return result;
+// }
+
+// console.log(getRandomString(10,20));
+
+
+// // return a string in ascending order
+// function getSortedString(string) {
+//     return string.split("").sort().join("");
+// }
+
+// Returns a random number between min (inclusive) and max (exclusive)
+const getRandomInteger = function(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+// return a random letter
+const getRandomLetter = function() {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    return alphabet[getRandomInteger(0,alphabet.length)];
+}
+
+// will return the random length string
+const getRandomString = function(minLength, maxLength) {
+    let result = "";
+
+    let lengthOfOutputString = getRandomInteger(minLength, maxLength);
+
+    for (let i = 0; i < lengthOfOutputString; i++) {
+        result += getRandomLetter();
+    }
+    return result;
+}
+
+console.log(getRandomString(10,20));
+
+// return a string in ascending order
+const getSortedString = function(string) {
+    return string.split("").sort().join("");
+}
+
+
